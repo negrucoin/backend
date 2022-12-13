36e2a51829f1972359f2bc119f80b3feb572cd5e
IvanPragma <ip20060226@gmail.com>
2022-12-13 22:02:33 +0500

    Register all models into django admin

--- negrucoin/main/admin.py
+++ negrucoin/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from main.models import Client
+from main.models import Client, MoneyTransfer, MoneySaleOffer, MoneyPurchase
 
 admin.site.register(Client)
+admin.site.register(MoneyTransfer)
+admin.site.register(MoneySaleOffer)
+admin.site.register(MoneyPurchase)
